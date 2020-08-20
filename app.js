b7715235df27b0df97b1d93ef4ae16df401905e0
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-08-20 19:35:44 -0400

    Fix for CORS issue

--- app.js
+++ app.js
@@ -35,6 +35,18 @@ app.use(
   })
 );
 
+// Setting up headers in case CORS fails
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 // Route setup
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
