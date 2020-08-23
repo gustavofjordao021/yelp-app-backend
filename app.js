3346e18d0b86874124c736e0d1534bb552e8ec20
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-08-23 10:14:39 -0400

    CORS configs

--- app.js
+++ app.js
@@ -30,19 +30,19 @@ app.use(
 );
 
 // Setting up headers in case CORS fails
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     `${process.env.REACT_APP_CLIENT_POINT}`
-//   );
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    `${process.env.REACT_APP_CLIENT_POINT}`
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
 
 // Middleware Setup
 app.use(helmet());
