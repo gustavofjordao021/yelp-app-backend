eb1358b5db2a5cc3a5b61e45414cf4fc52b9076c
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-08-23 10:56:22 -0400

    Removing console logs

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const app = express();
 
 // CORS setup
 app.use(
-  console.log("Origin =====> ", process.env.REACT_APP_CLIENT_POINT),
   cors({
     credentials: true,
     preflightContinue: true,
