7f0c7a6817eb50c7fbe33e9d9e6565cbafd290b4
gustavofjordao021 <gustavo.jordao021@gmail.com>
2020-09-12 17:30:08 -0400

    Work on Collections ticket

--- configs/session.config.js
+++ configs/session.config.js
@@ -1,43 +1,24 @@
-const session = require("cookie-session");
+const session = require("express-session");
 const mongoose = require("mongoose");
-// const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo")(session);
 
 module.exports = (app) => {
   app.use(
     session({
       secret: process.env.SESS_SECRET,
       name: "sessionID",
-      keys: [process.env.SESS_SECRET, process.env.SESS_SECRET],
-      sameSite: "lax",
-      // store: new MongoStore({
-      //   mongooseConnection: mongoose.connection,
-      //   ttl: 60 * 60 * 24 * 1000,
-      // }),
+      cookie: {
+        sameSite: "lax",
+      },
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 60 * 60 * 24 * 1000,
+      }),
     })
   );
 };
-
-// const session = require("express-session");
-// const mongoose = require("mongoose");
-// const MongoStore = require("connect-mongo")(session);
-
-// module.exports = (app) => {
-//   app.use(
-//     session({
-//       secret: process.env.SESS_SECRET,
-//       name: "sessionID",
-//       cookie: {
-//         sameSite: "lax",
-//       },
-//       resave: false,
-//       saveUninitialized: false,
-//       store: new MongoStore({
-//         mongooseConnection: mongoose.connection,
-//         ttl: 60 * 60 * 24 * 1000,
-//       }),
-//     })
-//   );
-// };
 
 // Previous code setting cookie configurations for session
 // module.exports = (app) => {
